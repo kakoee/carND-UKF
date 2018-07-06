68605d276433ac51fcf362c61051692f51634c02
Reza Kakoee <rkakoee@qti.qualcomm.com>
2018-07-05 17:40:14 -0700

    first version

---
+++ examples.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include "ukf.h"
+
+UKF::UKF() {
+  //TODO Auto-generated constructor stub
+  Init();
+}
+
+UKF::~UKF() {
+  //TODO Auto-generated destructor stub
+}
+
+void UKF::Init() {
+
+}
+
+/*******************************************************************************
+* Programming assignment functions: 
+*******************************************************************************/
+
+
+void UKF::GenerateSigmaPoints(MatrixXd* Xsig_out) {
+
+  //set state dimension
+  int n_x = 5;
+
+  //define spreading parameter
+  double lambda = 3 - n_x;
+
+  //set example state
+  VectorXd x = VectorXd(n_x);
+  x <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+  //set example covariance matrix
+  MatrixXd P = MatrixXd(n_x, n_x);
+  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+
+  //create sigma point matrix
+  MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1);
+
+  //calculate square root of P
+  MatrixXd A = P.llt().matrixL();
+
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+
+  //your code goes here 
+  Xsig.col(0) = x;
+  for(int i=1;i<n_x+1;i++)
+  {
+      Xsig.col(i) = (x + A.col(i-1) * sqrt(lambda+n_x));
+  }
+  for(int i=n_x+1;i<2*n_x+1;i++)
+  {
+      Xsig.col(i) = (x - A.col(i-n_x-1) * sqrt(lambda+n_x));
+  }
+  
+  //calculate sigma points ...
+  //set sigma points as columns of matrix Xsig
+
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
+
+  //print result
+  //std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+  //write result
+  *Xsig_out = Xsig;
+
+/* expected result:
+   Xsig =
+    5.7441  5.85768   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441
+      1.38  1.34566  1.52806     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38
+    2.2049  2.28414  2.24557  2.29582   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049
+    0.5015  0.44339 0.631886 0.516923 0.595227   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015
+    0.3528 0.299973 0.462123 0.376339  0.48417 0.418721 0.405627 0.243477 0.329261  0.22143 0.286879
+*/
+
+}
+
+#include <iostream>
+#include "Dense"
+#include <vector>
+#include "ukf.h"
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+int main() {
+
+	//Create a UKF instance
+	UKF ukf;
+
+/*******************************************************************************
+* Programming assignment calls
+*******************************************************************************/
+
+    MatrixXd Xsig = MatrixXd(5, 11);
+    ukf.GenerateSigmaPoints(&Xsig);
+
+    //print result
+    std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+	return 0;
+}
+
+
+#include <iostream>
+#include "ukf.h"
+
+UKF::UKF() {
+  //TODO Auto-generated constructor stub
+  Init();
+}
+
+UKF::~UKF() {
+  //TODO Auto-generated destructor stub
+}
+
+void UKF::Init() {
+
+}
+
+
+/*******************************************************************************
+* Programming assignment functions: 
+*******************************************************************************/
+
+void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
+
+  //set state dimension
+  int n_x = 5;
+
+  //set augmented dimension
+  int n_aug = 7;
+
+  //Process noise standard deviation longitudinal acceleration in m/s^2
+  double std_a = 0.2;
+
+  //Process noise standard deviation yaw acceleration in rad/s^2
+  double std_yawdd = 0.2;
+
+  //define spreading parameter
+  double lambda = 3 - n_aug;
+
+  //set example state
+  VectorXd x = VectorXd(n_x);
+  x <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+  //create example covariance matrix
+  MatrixXd P = MatrixXd(n_x, n_x);
+  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+
+  //create augmented mean vector
+  VectorXd x_aug = VectorXd(7);
+
+  //create augmented state covariance
+  MatrixXd P_aug = MatrixXd(7, 7);
+
+  //create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+
+/*******************************************************************************
+ * Student part begin
+ ******************************************************************************/
+ 
+  //create augmented mean state
+  x_aug.head(n_x)  = x;
+  x_aug[5] = 0;
+  x_aug[6] = 0;
+  //create augmented covariance matrix;
+  P_aug.topLeftCorner(n_x,n_x) = P;
+  P_aug.bottomRows(2) = MatrixXd::Zero(2, 7);
+  P_aug.rightCols(2) = MatrixXd::Zero(7,2);
+  P_aug(5,5) = std_a * std_a;
+  P_aug(6,6) = std_yawdd * std_yawdd;
+  //create square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+  //create augmented sigma points
+  Xsig_aug.col(0) =x_aug;
+  for(int i=1;i<n_aug+1;i++)
+  {
+      Xsig_aug.col(i) = (x_aug + A.col(i-1) * sqrt(lambda+n_aug));
+  }
+  for(int i=n_aug+1;i<2*n_aug+1;i++)
+  {
+      Xsig_aug.col(i) = (x_aug - A.col(i-n_aug-1) * sqrt(lambda+n_aug));
+  }
+  
+  
+/*******************************************************************************
+ * Student part end
+ ******************************************************************************/
+
+  //print result
+  std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+
+  //write result
+  *Xsig_out = Xsig_aug;
+
+/* expected result:
+   Xsig_aug =
+  5.7441  5.85768   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441
+    1.38  1.34566  1.52806     1.38     1.38     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38     1.38     1.38
+  2.2049  2.28414  2.24557  2.29582   2.2049   2.2049   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049   2.2049   2.2049
+  0.5015  0.44339 0.631886 0.516923 0.595227   0.5015   0.5015   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015   0.5015   0.5015
+  0.3528 0.299973 0.462123 0.376339  0.48417 0.418721   0.3528   0.3528 0.405627 0.243477 0.329261  0.22143 0.286879   0.3528   0.3528
+       0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641        0
+       0        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641
+*/
+
+}
+
+
+
